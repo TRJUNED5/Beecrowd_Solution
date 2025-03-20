1207f70132834e38247f855dee520a95b979f256
TazanRabbani <161732714+TRJUNED5@users.noreply.github.com>
2025-03-20 18:03:58 -0400

    Create 1038.c

---
+++ 1038.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    float Z;
+    scanf("%d %d",&X,&Y);
+    if(X == 1)
+     Z=Y*4.00;
+    if(X == 2)
+     Z=Y*4.50;
+    if(X == 3)
+     Z=Y*5.00;
+    if(X == 4)
+     Z=Y*2.00;
+    if(X == 5)
+     Z=Y*1.50;
+    printf("Total: R$ %.2f\n",Z);
+    return 0;
+}
