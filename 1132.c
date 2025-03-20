5920abd7f1b7fff64dfb0994a146985f71981521
TazanRabbani <161732714+TRJUNED5@users.noreply.github.com>
2025-03-20 18:24:33 -0400

    Create 1132.c

---
+++ 1132.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int X,Y,i,j,temp,sum = 0;
+    scanf("%d %d",&X,&Y);
+    if(X>Y){temp=X;X=Y;Y=temp;}
+    for(i=X;i<=Y;i++){
+        if(i % 13 != 0){
+            sum += i;
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
+}
